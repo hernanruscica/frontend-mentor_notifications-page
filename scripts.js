a9bf380c140941a0a6f7c11a50b0c52d29db7d6e
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-10-20 22:54:58 -0300

    Add files via upload
    finished

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 console.log("from js");
 
-/*DOM variables */
+/*DOM variables */ 
 const $d = document;
 const $spanNumberUnreads = $d.getElementById("number-unreads");
 const $aMarkAsRead = $d.getElementById("mark-as-read");
@@ -10,32 +10,24 @@ const foundClasses = (className) => {
     const $classesFounded = $d.querySelectorAll('.' + className);       
     return $classesFounded
 }
-let nodeClasses = foundClasses("notifications__container__item");
-console.log(nodeClasses.length);
-
-
-const getNumberUnreads = () => {
-    let counterUnreads = 0;
-    nodeClasses.forEach((element)=> {
-        if (element.classList.contains('unread')){
-            counterUnreads += 1;
-        }
-    })
-    return counterUnreads;
-}
-let unreadNotifications = getNumberUnreads();
-console.log(unreadNotifications);
 
 const updateNotificationsNumber = () => {
+    let $nodeClasses = foundClasses("unread");
+    let unreadNotificationsQuantity = $nodeClasses.length;
     $spanNumberUnreads.innerHTML = "";
-    $spanNumberUnreads.innerHTML = getNumberUnreads();
+    $spanNumberUnreads.innerHTML = unreadNotificationsQuantity; 
 }
 
-/*
+updateNotificationsNumber();
 
 $aMarkAsRead.addEventListener('click', (e) => {
     e.preventDefault();
     console.log('click on the Mark all as read button');
-    const $classesFounded = $d.querySelectorAll('notifications__container__item');
+    let $unreadNotifications = foundClasses("unread");
+    $unreadNotifications.forEach(($notification) => {
+        console.log($notification);
+        $notification.classList.remove('unread');
+        $notification.classList.add('read');
+        updateNotificationsNumber();
+    });
 });
-*/
